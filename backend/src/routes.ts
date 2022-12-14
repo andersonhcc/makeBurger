44a72ceacb3ccca3b7968b7626d8a5c17ea80dc4
andersonhcc <programadorandersonh@gmail.com>
2022-12-14 19:50:11 -0300

    create category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,17 +1,26 @@
 import { Router } from 'express';
+
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailsUserController } from './controllers/user/DetailsUserController';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
+
+// -- routes user --
 
 router.post('/users', new CreateUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
 
 router.get('/me', isAuthenticated, new DetailsUserController().handle);
+
+// -- routes category --
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
 
 
 export { router };  
