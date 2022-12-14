dd055ba074770243a26c675744cc67ae97122b46
andersonhcc <programadorandersonh@gmail.com>
2022-12-14 14:20:14 -0300

    middlewares isAuthenticated + routes private

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,17 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailsUserController } from './controllers/user/DetailsUserController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
+
+router.get('/me', isAuthenticated, new DetailsUserController().handle);
+
 
 export { router };  
